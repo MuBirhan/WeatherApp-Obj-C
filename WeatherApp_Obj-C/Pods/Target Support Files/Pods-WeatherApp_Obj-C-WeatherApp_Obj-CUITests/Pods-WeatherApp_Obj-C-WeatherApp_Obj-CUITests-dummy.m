19701c83c510a3bd08402b5f22a204a3dbd03d97
MuBirhan <birhan.muradasil@rodeapps.com>
2020-02-17 12:01:22 +0200

    removed pods

--- WeatherApp_Obj-C/Pods/Target Support Files/Pods-WeatherApp_Obj-C-WeatherApp_Obj-CUITests/Pods-WeatherApp_Obj-C-WeatherApp_Obj-CUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeatherApp_Obj_C_WeatherApp_Obj_CUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WeatherApp_Obj_C_WeatherApp_Obj_CUITests
-@end
