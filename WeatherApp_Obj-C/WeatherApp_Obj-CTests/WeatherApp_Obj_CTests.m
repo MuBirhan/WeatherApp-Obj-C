7e9f11d8148effdc92f261eeb2a4ee60422b8478
MuBirhan <birhan.muradasil@rodeapps.com>
2020-02-17 12:30:35 +0200

    readded all files

---
+++ WeatherApp_Obj-C/WeatherApp_Obj-CTests/WeatherApp_Obj_CTests.m
@@ -0,0 +1,37 @@
+//
+//  WeatherApp_Obj_CTests.m
+//  WeatherApp_Obj-CTests
+//
+//  Created by Muradasil Birhan on 13/02/2020.
+//  Copyright © 2020 RodeApps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WeatherApp_Obj_CTests : XCTestCase
+
+@end
+
+@implementation WeatherApp_Obj_CTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
