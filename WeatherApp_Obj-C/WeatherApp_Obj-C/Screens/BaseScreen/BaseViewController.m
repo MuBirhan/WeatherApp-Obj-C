742e8fee090c1df85cb1777efafe967344309df1
MuBirhan <birhan.muradasil@rodeapps.com>
2020-02-20 14:11:37 +0200

    Fixed some constraints

--- WeatherApp_Obj-C/WeatherApp_Obj-C/Screens/BaseScreen/BaseViewController.m
+++ WeatherApp_Obj-C/WeatherApp_Obj-C/Screens/BaseScreen/BaseViewController.m
@@ -46,14 +46,4 @@
     [indicator stopLoading];
     [indicator removeFromSuperview];
 }
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
-
 @end
